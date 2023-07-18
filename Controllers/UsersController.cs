9c1862fd7e735af9c4abc7e578bde6f3e990d575
richardr <richardr@wsb.co.za>
2023-07-18 12:08:31 +0200

    setting up registrationa and login fixes

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace LottoApi.Controllers
             _collection = _dbContext.Users;
         }
 
-        [HttpGet]
+        [HttpGet("/api/Users")]
         public async Task<ActionResult<IEnumerable<Users>>> GetAllUsers()
         {
             var users = await _collection.Find(u => true).ToListAsync();
@@ -37,12 +37,52 @@ namespace LottoApi.Controllers
             return Ok(user);
         }
 
-        [HttpPost]
-        public async Task<ActionResult<Users>> CreateUser(Users user)
+        [HttpPost("login")]
+        public async Task<ActionResult<UserLoginRequest>> Login(UserLoginRequest request)
         {
+            var user = await _collection.Find(u => u.Email == request.Email && u.Password == request.Password).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                // User not found or invalid credentials
+                return Unauthorized();
+            }
+
+            // Create a UserLoginResponse object with the necessary data
+            var response = new UserLoginRequest
+            {
+                Email = user.Email,
+                Password = user.Password,
+                // Include any other properties you want to return in the response
+            };
+
+            return Ok(response);
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<Users>> RegisterUser(RegisterRequest request)
+        {
+            var existingUser = await _collection.Find(u => u.Email == request.Email).FirstOrDefaultAsync();
+            if (existingUser != null)
+            {
+                // User with the same email already exists
+                return Conflict("User with the same email already exists.");
+            }
+
+            var user = new Users
+            {
+                Name = request.Name,
+                Surname = request.Surname,
+                Email = request.Email,
+                Password = request.Password,
+                IdentityNumber = request.IdentityNumber
+                // Include any other properties you want to set for the user
+            };
+
             await _collection.InsertOneAsync(user);
             return CreatedAtAction(nameof(GetUserById), new { id = user._id }, user);
         }
+
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Users>> UpdateUser(string id, Users user)
